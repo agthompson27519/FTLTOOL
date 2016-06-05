0d0e93e218c6d2e8a510fc9ef5efa02020919fa2
Gardner Thompson <agthompson27519@gmail.com>
2016-06-05 01:09:59 +0000

    Add files via upload

---
+++ FTL SaveScummer/FTL SaveScummer/FTL.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace FTL_SaveScummer
+{
+    partial class FTL
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FTL));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buyFTLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buyIOSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buyDRMFreeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buySteamToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runFTLSteamToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnBackup = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.lbSaves = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.buyFTLToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.runFTLSteamToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1264, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // buyFTLToolStripMenuItem
+            // 
+            this.buyFTLToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.buyIOSToolStripMenuItem,
+            this.buyDRMFreeToolStripMenuItem,
+            this.buySteamToolStripMenuItem});
+            this.buyFTLToolStripMenuItem.Name = "buyFTLToolStripMenuItem";
+            this.buyFTLToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.buyFTLToolStripMenuItem.Text = "Buy FTL";
+            // 
+            // buyIOSToolStripMenuItem
+            // 
+            this.buyIOSToolStripMenuItem.Name = "buyIOSToolStripMenuItem";
+            this.buyIOSToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.buyIOSToolStripMenuItem.Text = "Buy iOS";
+            this.buyIOSToolStripMenuItem.Click += new System.EventHandler(this.buyIOSToolStripMenuItem_Click);
+            // 
+            // buyDRMFreeToolStripMenuItem
+            // 
+            this.buyDRMFreeToolStripMenuItem.Name = "buyDRMFreeToolStripMenuItem";
+            this.buyDRMFreeToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.buyDRMFreeToolStripMenuItem.Text = "Buy DRM-Free";
+            this.buyDRMFreeToolStripMenuItem.Click += new System.EventHandler(this.buyDRMFreeToolStripMenuItem_Click);
+            // 
+            // buySteamToolStripMenuItem
+            // 
+            this.buySteamToolStripMenuItem.Name = "buySteamToolStripMenuItem";
+            this.buySteamToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.buySteamToolStripMenuItem.Text = "Buy Steam";
+            this.buySteamToolStripMenuItem.Click += new System.EventHandler(this.buySteamToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // runFTLSteamToolStripMenuItem
+            // 
+            this.runFTLSteamToolStripMenuItem.Name = "runFTLSteamToolStripMenuItem";
+            this.runFTLSteamToolStripMenuItem.Size = new System.Drawing.Size(114, 20);
+            this.runFTLSteamToolStripMenuItem.Text = "Run FTL <Steam>";
+            this.runFTLSteamToolStripMenuItem.Click += new System.EventHandler(this.runFTLSteamToolStripMenuItem_Click);
+            // 
+            // btnBackup
+            // 
+            this.btnBackup.BackColor = System.Drawing.Color.Transparent;
+            this.btnBackup.Location = new System.Drawing.Point(1052, 569);
+            this.btnBackup.Name = "btnBackup";
+            this.btnBackup.Size = new System.Drawing.Size(200, 100);
+            this.btnBackup.TabIndex = 1;
+            this.btnBackup.Text = "Backup Current Save";
+            this.btnBackup.UseVisualStyleBackColor = false;
+            this.btnBackup.Click += new System.EventHandler(this.btnBackup_Click);
+            // 
+            // btnImport
+            // 
+            this.btnImport.BackColor = System.Drawing.Color.Transparent;
+            this.btnImport.Location = new System.Drawing.Point(1052, 463);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(200, 100);
+            this.btnImport.TabIndex = 2;
+            this.btnImport.Text = "Import Selected Save";
+            this.btnImport.UseVisualStyleBackColor = false;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // lbSaves
+            // 
+            this.lbSaves.FormattingEnabled = true;
+            this.lbSaves.Location = new System.Drawing.Point(1052, 27);
+            this.lbSaves.Name = "lbSaves";
+            this.lbSaves.Size = new System.Drawing.Size(200, 433);
+            this.lbSaves.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 668);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "v0.1";
+            // 
+            // FTL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FTL_SaveScummer.Properties.Resources.ftlbg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbSaves);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnBackup);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximumSize = new System.Drawing.Size(1280, 720);
+            this.MinimumSize = new System.Drawing.Size(1280, 720);
+            this.Name = "FTL";
+            this.Text = "FTL SaveScummer";
+            this.Load += new System.EventHandler(this.FTL_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buyFTLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buyIOSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buyDRMFreeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buySteamToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Button btnBackup;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.ListBox lbSaves;
+        private System.Windows.Forms.ToolStripMenuItem runFTLSteamToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FTL SaveScummer/FTL SaveScummer/FTL.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FTL_SaveScummer
+{
+    public partial class FTL : Form
+    {
+        public FTL()
+        {
+            InitializeComponent();
+        }
+        string currentsavpath = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\My Games\\fasterthanlight\\continue.sav";
+        string tempdirpath = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\FTLsavescummer";
+        List<string> backups = new List<string>();
+        string currenttime, selectedsave;
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            selectedsave = lbSaves.SelectedItem.ToString();
+            File.Delete(currentsavpath);
+            selectedsave = selectedsave.Replace("/", "x");
+            selectedsave = selectedsave.Replace(":", "y");
+            selectedsave = selectedsave.Replace(" ", "z");
+            File.Copy(tempdirpath + "\\" + selectedsave + "\\continue.sav", currentsavpath);
+        }
+        private void btnBackup_Click(object sender, EventArgs e)
+        {
+            currenttime = DateTime.Now.ToString();
+            currenttime = currenttime.Replace("/","x");
+            currenttime = currenttime.Replace(":","y");
+            currenttime = currenttime.Replace(" ", "z");
+            if (!Directory.Exists(tempdirpath + "\\" + currenttime))
+            {
+                Directory.CreateDirectory(tempdirpath + "\\" + currenttime);
+                try
+                {
+                    File.Copy(currentsavpath, tempdirpath + "\\" + currenttime + "\\continue.sav"); //copies the save file to a backup directory
+                    currenttime = currenttime.Replace("x", "/");
+                    currenttime = currenttime.Replace("y", ":");
+                    currenttime = currenttime.Replace("z", " ");
+                    backups.Add(currenttime); //add the current time to the list (NOT listbox)
+                    lbSaves.Items.Add(currenttime); //adds the current time to the listbox
+                    currenttime = currenttime.Replace("/", "x");
+                    currenttime = currenttime.Replace(":", "y");
+                    currenttime = currenttime.Replace(" ", "z");
+                }
+                catch
+                {
+                    MessageBox.Show("could not find file 'continue.sav' @" + currentsavpath,"error 1"); //displays messagebox telling the user that the program cannot find their continue.sav file in the default directory where it would normally be
+                }                   
+                File.WriteAllLines(tempdirpath + "\\backups.txt", backups); //write the current list of backups to backups.txt in My Documents        
+            }            
+        }
+        private void buyIOSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://itunes.apple.com/us/app/ftl-faster-than-light/id833951143?mt=8"); //default browser with link to iTunes APPSTORE page for FTL
+        }
+        private void buyDRMFreeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.humblebundle.com/store/ftl-faster-than-light"); //open default browser with link to humble store page for FTL
+        }
+        private void buySteamToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://store.steampowered.com/app/212680/"); //open default browser with link to steam store page for FTL
+        }
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Help\r\n\r\n'error 1': there is no current save to back up, a 'continue.sav' file is not found at the path\r\n\r\n'I have the steam version and saves will not import correctly'\r\nTurn off steam cloud for this game by right clicking FTL: Faster Than Light in your steam library, click 'properties', navigate to the 'updates' tab, and at the bottom of the form, uncheck the box next to 'Enable Steam synchronization...' and the import function should work.","Help");
+        }
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("FTL: Save Scummer\r\n\r\nThis is a program I made to import/export save files from FTL: Faster Than Light, which is a roguelike space-fantasy game realeased by Subset Games for iOS, Linux, Windows, and OSX. Being a roguelike, upon death, saves are deleted, sometimes referred to as 'permadeath'. This program aims to help users circumvent the permadeath system by exploiting the fact that the game allows you to temporarily save your progress at any point by pressing escape and choosing to 'Save & Quit'. The program copies or 'Backs up' your current save progress, and then copies it back when you click 'Import'.\r\n\r\nMade by Gardner Thompson, 2016, using Visual Studio 2015 Community, using C#.\r\nThanks to Subset games for creating FTL: Faster Than Light.","About");
+        }
+        private void FTL_Load(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(tempdirpath))
+                Directory.CreateDirectory(tempdirpath);
+            if (!File.Exists(tempdirpath + "\\backups.txt"))
+            {
+                File.Create(tempdirpath + "\\backups.txt");
+                MessageBox.Show("initialized, please re-open the program to use", "initialized"); //prompt the user to re-open the program
+                Environment.Exit(0); //closes program
+            }
+            backups.AddRange(File.ReadAllLines(tempdirpath + "\\backups.txt")); //adding text identifiers in the list of time of backups
+            lbSaves.Items.AddRange(File.ReadAllLines(tempdirpath + "\\backups.txt")); //adding these items to the listbox
+        }
+        private void runFTLSteamToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(@"C:\\Program Files (x86)\\Steam\\steamapps\\common\\FTL Faster Than Light")) //check if FTL is installed through steam
+            {
+                System.Diagnostics.Process.Start("steam://rungameid/212680"); //run FTL through steam (if that's where it is installed)
+            }
+            
+        }
+    }
+}
